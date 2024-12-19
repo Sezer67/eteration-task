f8ae1482458b8e87746502bcc615fdcc302151d3
inveon-sezerkenar <sezer.kenar@inveon.com>
2024-12-19 15:15:27 +0300

    setup

---
+++ package.sh
@@ -0,0 +1,16 @@
+rm -rf node_modules
+rm package-lock.json
+echo $"old files deleted"
+
+npm i
+echo $"npm packages was done"
+
+bundle install --path vendor/bundle
+cd ios
+rm -rf Pods
+rm -rf Podfile.lock
+
+bundle exec pod install
+echo $"pods was done"
+
+cd ..
